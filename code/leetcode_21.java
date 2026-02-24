2f603cfc12ac6ad1f692f7e777f42393ad1cd205
iahgig <1286354792@qq.com>
2026-02-24 22:05:41 +0800

    2026.2.24

---
+++ code/leetcode_21.java
@@ -0,0 +1,24 @@
+public class leetcode_21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {// 维护新链表head，tail，不断取两个链表的较小节点插入到tail，并移动相应指针
+        ListNode prehead = new ListNode(-1);
+        ListNode tail = prehead;
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                tail.next = p1;
+                p1 = p1.next;
+            } else {
+                tail.next = p2;
+                p2 = p2.next;
+            }
+            tail = tail.next;
+        }
+        if (p1 == null) {
+            tail.next = p2;
+        } else {
+            tail.next = p1;
+        }
+        return prehead.next;
+    }
+}
